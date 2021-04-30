6997f2be195f80dc5e5b53e40fcf9de37145bcfa
stanfinance <stan.finance20@gmail.com>
2021-04-30 18:42:25 +0530

    update right

--- static/js/18.be09bc4e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpprintershares-frontend"]=this["webpackJsonpprintershares-frontend"]||[]).push([[18],{775:function(n,s){}}]);
-//# sourceMappingURL=18.be09bc4e.chunk.js.map
--- static/js/18.defd0847.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoose-frontend"]=this["webpackJsonpgoose-frontend"]||[]).push([[18],{775:function(o,n){}}]);
-//# sourceMappingURL=18.defd0847.chunk.js.map
